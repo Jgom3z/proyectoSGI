6ee8aa040beebd1fc02092c62084a721f6696b49
Jgom3z <jgomezpusb@gmail.com>
2024-08-29 10:04:06 -0500

    Avances en CRUD
    Se avanzo crud de grupos de investigacion pero aun no se realizan pruebas, se crearon las funciones pero no se han implementado en el html

--- SGI/vista/vistaGruposInvestigacion.py
+++ SGI/vista/vistaGruposInvestigacion.py
@@ -143,72 +143,83 @@ def create_grupo():
         error_message = response.json().get("message", "Error desconocido al guardar los datos")
         return jsonify({"message": f"Error al guardar los datos: {error_message}"}), 500
     
-# from pprint import pprint
-# from flask import Blueprint, request, render_template, redirect, url_for
-# from controlador.ControlEntidad import ControlEntidad
+@vistaGruposInvestigacion.route("/deletegrupo", methods = ['POST'])
+def delete_grupo():
+    # Captura los datos del formulario enviado
+    print(request)
+    form_data = {
+        "nombre_grupo": request.form.get('nombre_grupo')  
+    } 
 
-# Crear un Blueprint
-# vistaGruposInvestigacion = Blueprint('idVistaGruposInvestigacion', __name__, template_folder='templates')
+     # Debug: Imprimir el formulario de datos
+    print("Form Data: ", form_data)  
 
-# @vistaGruposInvestigacion.route('/vistaGruposInvestigacion', methods=['GET', 'POST'])
-# def vista_GruposInvestigacion():
-#     mensaje = ""
-#     objControlEntidad = ControlEntidad('grupo_investigacion')
-#     arregloGrupos = objControlEntidad.select_data()
+    # Enviar los datos a la API
+    response = requests.post(API_URL.format(projectName=projectName), json={
+        "procedure": "delete_json_entity",  # Supongamos que tienes un procedimiento almacenado para insertar
+        "parameters":{
+            "table_name":"inv_grupos",
+            "where_condition": "nombre_grupo = nombre_grupo"
+                
+        }
+    } 
+    )
+
+       # Debug: Imprimir el estado de la respuesta y su contenido
+    print("Response Status Code: ", response.status_code)
+    print("Response Content: ", response.content)
+
+    # Verificar la respuesta de la API
+    if response.status_code == 200:
+        return jsonify({"message": "Grupo Eliminado exitosamente"}), 200
+    else:
+        # Mostrar el mensaje de error detallado
+        error_message = response.json().get("message", "Error desconocido al eliminar el grupo")
+        return jsonify({"message": f"Error al eliminar el grupo: {error_message}"}), 500
     
-#     boton = request.form.get('bt', '')
-#     id_grupo = request.form.get('txtIdGrupo', '')
-#     nombre_grupo = request.form.get('txtNombreGrupo', '')
-#     codigo_grup_lac = request.form.get('txtCodigoGrupoLAC', '')
-#     categoria_colciencias = request.form.get('txtCategoriaColciencias', '')
-#     area_conocimiento = request.form.get('txtAreaConocimiento', '')
-#     id_facultad = request.form.get('txtIdFacultad', '')
-#     fecha_creacion = request.form.get('txtFechaCreacion', '')
-#     fecha_finalizacion = request.form.get('txtFechaFinalizacion', '')
-#     id_lider = request.form.get('txtIdLider', '')
-#     plan_estrategico = request.form.get('txtPlanEstrategico', '')
-#     categoria_meta = request.form.get('txtCategoriaMeta', '')
-#     estrategia_meta = request.form.get('txtEstrategiaMeta', '')
-#     vision = request.form.get('txtVision', '')
-#     objetivos = request.form.get('txtObjetivos', '')
+@vistaGruposInvestigacion.route("/updategrupo", methods = ['POST'])
+def update_grupo():
+    # Captura los datos del formulario enviado
+    print(request)
+    form_data = {
+        "nombre_grupo": request.form.get('nombre_grupo'),
+        "codigo_grup_lac": request.form.get('codigo_grup_lac'),
+        "categoria_colciencias": request.form.get('categoria_colciencias'),
+        "area_conocimiento": request.form.get('area_conocimiento'),
+        "id_facultad": request.form.get('id_facultad'),
+        "fecha_creacion": request.form.get('fecha_creacion'),
+        "fecha_finalizacion": request.form.get('fecha_finalizacion'),
+        "id_lider": request.form.get('id_lider'),
+        "plan_estrategico": request.form.get('plan_estrategico'),
+        "categoria_meta": request.form.get('categoria_meta'),
+        "estrategia_meta": request.form.get('estrategia_meta'),
+        "vision": request.form.get('vision'),
+        "objetivos": request.form.get('objetivos')
+    }  
 
-#     datosGrupo = {
-#         'id_grupo': id_grupo,
-#         'nombre_grupo': nombre_grupo,
-#         'codigo_grup_lac': codigo_grup_lac,
-#         'categoria_colciencias': categoria_colciencias,
-#         'area_conocimiento': area_conocimiento,
-#         'id_facultad': id_facultad,
-#         'fecha_creacion': fecha_creacion,
-#         'fecha_finalizacion': fecha_finalizacion,
-#         'id_lider': id_lider,
-#         'plan_estrategico': plan_estrategico,
-#         'categoria_meta': categoria_meta,
-#         'estrategia_meta': estrategia_meta,
-#         'vision': vision,
-#         'objetivos': objetivos
-#     }
+     # Debug: Imprimir el formulario de datos
+    print("Form Data: ", form_data)  
 
-#     if boton == 'Guardar':
-#         objGrupo = Entidad(datosGrupo)
-#         objControlEntidad.insert_data(objGrupo)
-#         return redirect(url_for('idVistaGruposInvestigacion.vista_GruposInvestigacion'))
-    
-#     elif boton == 'Consultar':
-#         objGrupo = objControlEntidad.buscarPorId('id_grupo', id_grupo)
-#         if objGrupo:
-#             datosGrupo = objGrupo.__dict__
-#         else:
-#             mensaje = "El Grupo de Investigación no se encontró."
-    
-#     elif boton == 'Modificar':
-#         objGrupo = Entidad(datosGrupo)
-#         objControlEntidad.update_data('id_grupo', id_grupo, objGrupo)
-#         return redirect(url_for('idVistaGruposInvestigacion.vista_GruposInvestigacion'))
-    
-#     elif boton == 'Borrar':
-#         objControlEntidad.delete_data('id_grupo', id_grupo)
-#         return redirect(url_for('idVistaGruposInvestigacion.vista_GruposInvestigacion'))
+    # Enviar los datos a la API
+    response = requests.post(API_URL.format(projectName=projectName), json={
+        "procedure": "delete_json_entity",  # Supongamos que tienes un procedimiento almacenado para insertar
+        "parameters":{
+            "table_name":"inv_grupos",
+            "json_data": form_data,
+            "where_condition": "nombre_grupo = nombre_grupo"
+                
+        }
+    } 
+    )
 
-#     # Renderizar la plantilla al final, pasando las variables necesarias
-#     return render_template('vistaGruposInvestigacion.html', arregloGrupos=arregloGrupos, mensaje=mensaje, grupo=datosGrupo)
+       # Debug: Imprimir el estado de la respuesta y su contenido
+    print("Response Status Code: ", response.status_code)
+    print("Response Content: ", response.content)
+
+    # Verificar la respuesta de la API
+    if response.status_code == 200:
+        return jsonify({"message": "Grupo actualizado exitosamente"}), 200
+    else:
+        # Mostrar el mensaje de error detallado
+        error_message = response.json().get("message", "Error desconocido al actualizar el grupo")
+        return jsonify({"message": f"Error al actualizar el grupo: {error_message}"}), 500
