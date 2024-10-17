2fe06c6669871d6aedb685f682334871d07c1bc9
Marlon Garcia <101010203+Marlon023@users.noreply.github.com>
2024-10-17 16:10:30 -0500

    estabilizando rama marlon

--- SGI/vista/select_list.py
+++ SGI/vista/select_list.py
@@ -85,4 +85,143 @@ def estudiantes():
         result = []
     return result
 
+def grupos():    
+    select_data = {
+            "projectName": projectName,
+            "procedure": "select_json_entity",
+            "parameters": {
+                "table_name": "inv_grupos",
+                "json_data": {},
+                "where_condition": "",
+                "select_columns": "id_grupo, nombre_grupo",
+                "order_by": "nombre_grupo",
+                "limit_clause": ""
+            }
+        }
+    
+    response = requests.post(API_URL, json=select_data)
+    if response.status_code != 200:
+        return f"Error al consultar la API: {response.status_code}"
+
+    data = response.json()
+    if 'result' in data and data['result']:
+        data_str = data['result'][0]['result']
+        result = json.loads(data_str)
+    else:
+        result = []
+    
+    return result
+
+def facultad():
+    select_data = {
+        "projectName": projectName,
+        "procedure": "select_json_entity",
+        "parameters": {
+            "table_name": "inv_facultad",
+            "json_data": {},
+            "where_condition": "",
+            "select_columns": "id_facultad, nombre_facultad",
+            "order_by": "nombre_facultad",
+            "limit_clause": ""
+        }
+    }
+
+    # Realiza la solicitud a la API
+    response = requests.post(API_URL, json=select_data)
+
+    # Verifica si la solicitud fue exitosa
+    if response.status_code != 200:
+        return f"Error al consultar la API: {response.status_code}"
+
+    # Verifica si el contenido de la respuesta es válido
+    data = response.json()
+    if 'result' in data and data['result']:
+        data_str = data['result'][0].get('result')  # Utiliza get() para evitar errores si 'result' es None
+        if data_str:  # Verifica que data_str no sea None o vacío
+            try:
+                result = json.loads(data_str)
+            except json.JSONDecodeError:
+                return "Error al decodificar el JSON de la API."
+        else:
+            result = []  # Si data_str es None, devuelve una lista vacía
+    else:
+        result = []
+
+    return result
+
+def productos():
+    select_data = {
+        "projectName": projectName,
+        "procedure": "select_json_entity",
+        "parameters": {
+            "table_name": "inv_producto",
+            "json_data": {},
+            "where_condition": "",
+            "select_columns": "id_producto, subtipo",
+            "order_by": "subtipo",
+            "limit_clause": ""
+        }
+    }
+
+    # Realiza la solicitud a la API
+    response = requests.post(API_URL, json=select_data)
+
+    # Verifica si la solicitud fue exitosa
+    if response.status_code != 200:
+        return f"Error al consultar la API: {response.status_code}"
+
+    # Verifica si el contenido de la respuesta es válido
+    data = response.json()
+    if 'result' in data and data['result']:
+        data_str = data['result'][0].get('result')  # Utiliza get() para evitar errores si 'result' es None
+        if data_str:  # Verifica que data_str no sea None o vacío
+            try:
+                result = json.loads(data_str)
+            except json.JSONDecodeError:
+                return "Error al decodificar el JSON de la API."
+        else:
+            result = []  # Si data_str es None, devuelve una lista vacía
+    else:
+        result = []
+
+    return result
+
+def cofinanciadores():
+    select_data = {
+        "projectName": projectName,
+        "procedure": "select_json_entity",
+        "parameters": {
+            "table_name": "inv_cofinanciador",
+            "json_data": {},
+            "where_condition": "",
+            "select_columns": "id_cofinanciador, institucion",
+            "order_by": "institucion",
+            "limit_clause": ""
+        }
+    }
+
+    # Realiza la solicitud a la API
+    response = requests.post(API_URL, json=select_data)
+
+    # Verifica si la solicitud fue exitosa
+    if response.status_code != 200:
+        return f"Error al consultar la API: {response.status_code}"
+
+    # Verifica si el contenido de la respuesta es válido
+    data = response.json()
+    if 'result' in data and data['result']:
+        data_str = data['result'][0].get('result')  # Utiliza get() para evitar errores si 'result' es None
+        if data_str:  # Verifica que data_str no sea None o vacío
+            try:
+                result = json.loads(data_str)
+            except json.JSONDecodeError:
+                return "Error al decodificar el JSON de la API."
+        else:
+            result = []  # Si data_str es None, devuelve una lista vacía
+    else:
+        result = []
+
+    return result
+
+
 
